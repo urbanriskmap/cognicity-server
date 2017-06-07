d817e3457ee3422131af4cb7e48a8d8983154238
Tomas <tomasholderness@gmail.com>
2017-06-07 17:36:30 -0400

    fixed tabbing

--- src/api/routes/floodgauges/index.js
+++ src/api/routes/floodgauges/index.js
@@ -12,45 +12,45 @@ import validate from 'celebrate';
 
 
 export default ({ config, db, logger }) => {
-	let api = Router();
+  let api = Router();
 
-	// Get a list of all flood gauge reports
-	api.get('/', cacheResponse('1 minute'),
-		validate({
-			query: {
-				city: Joi.any().valid(config.REGION_CODES),
-				format: Joi.any().valid(config.FORMATS).default(config.FORMAT_DEFAULT),
-				geoformat: Joi.any().valid(config.GEO_FORMATS).default(config.GEO_FORMAT_DEFAULT)
-			}
-		}),
-		(req, res, next) => floodgauges(config, db, logger).all()
-			.then((data) => handleGeoResponse(data, req, res, next))
-			.catch((err) => {
-				/* istanbul ignore next */
-				logger.error(err);
-				/* istanbul ignore next */
-				next(err);
-			})
+  // Get a list of all flood gauge reports
+  api.get('/', cacheResponse('1 minute'),
+    validate({
+      query: {
+        city: Joi.any().valid(config.REGION_CODES),
+        format: Joi.any().valid(config.FORMATS).default(config.FORMAT_DEFAULT),
+        geoformat: Joi.any().valid(config.GEO_FORMATS).default(config.GEO_FORMAT_DEFAULT)
+      }
+    }),
+    (req, res, next) => floodgauges(config, db, logger).all()
+      .then((data) => handleGeoResponse(data, req, res, next))
+      .catch((err) => {
+        /* istanbul ignore next */
+        logger.error(err);
+        /* istanbul ignore next */
+        next(err);
+      })
   );
 
-	// Get a single flood gauge report
-	api.get('/:id', cacheResponse('1 minute'),
-		validate({
-			params: { id: Joi.number().integer().required() } ,
-			query: {
-				format: Joi.any().valid(config.FORMATS).default(config.FORMAT_DEFAULT),
-				geoformat: Joi.any().valid(config.GEO_FORMATS).default(config.GEO_FORMAT_DEFAULT)
-			}
-		}),
-		(req, res, next) => floodgauges(config, db, logger).byId(req.params.id)
-			.then((data) => handleGeoResponse(data, req, res, next))
-			.catch((err) => {
-				/* istanbul ignore next */
-				logger.error(err);
-				/* istanbul ignore next */
-				next(err);
-			})
-	);
+  // Get a single flood gauge report
+  api.get('/:id', cacheResponse('1 minute'),
+    validate({
+      params: { id: Joi.number().integer().required() } ,
+      query: {
+        format: Joi.any().valid(config.FORMATS).default(config.FORMAT_DEFAULT),
+        geoformat: Joi.any().valid(config.GEO_FORMATS).default(config.GEO_FORMAT_DEFAULT)
+      }
+    }),
+    (req, res, next) => floodgauges(config, db, logger).byId(req.params.id)
+      .then((data) => handleGeoResponse(data, req, res, next))
+      .catch((err) => {
+        /* istanbul ignore next */
+        logger.error(err);
+        /* istanbul ignore next */
+        next(err);
+      })
+  );
 
-	return api;
+  return api;
 };

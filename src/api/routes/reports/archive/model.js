e9b9dce8913311f6c402795a6e910827f59600a4
Tomas <tomasholderness@gmail.com>
2017-06-28 12:21:59 -0400

    removed unused byId function

--- src/api/routes/reports/archive/model.js
+++ src/api/routes/reports/archive/model.js
@@ -27,25 +27,5 @@ export default (config, db, logger) => ({
 		db.any(query, values).timeout(config.PGTIMEOUT)
 			.then((data) => resolve(data))
 			.catch((err) => reject(err));
-	}),
-
-	// Return specific report by id
-	byId: (id) => new Promise((resolve, reject) => {
-
-		// Setup query
-		let query = `SELECT pkey, created_at, source,
-			status, url, image_url, disaster_type, report_data, tags, title, text, the_geom
-			FROM ${config.TABLE_REPORTS}
-			WHERE pkey = $1`;
-
-		// Setup values
-		let values = [ id ];
-
-		// Execute
-		logger.debug(query, values);
-		db.oneOrNone(query, values).timeout(config.PGTIMEOUT)
-			.then((data) => resolve(data))
-			.catch((err) => reject(err));
 	})
-
 });

076bcfe84473d58773b71f2c799d1790c5d90ec9
Tomas <tomasholderness@gmail.com>
2017-06-28 14:29:47 -0400

    changed tabs to spaces

--- src/api/routes/cards/index.js
+++ src/api/routes/cards/index.js
@@ -22,13 +22,13 @@ const CACHE_GROUP_CARDS = '/cards';
 
 // Function to clear out the cache
 const clearCache = () => {
-	apicache.clear(CACHE_GROUP_CARDS);
+  apicache.clear(CACHE_GROUP_CARDS);
 };
 
 export default ({ config, db, logger }) => {
 
   // Router
-	let api = Router();
+  let api = Router();
 
   // Create an S3 object
   let s3 = new AWS.S3(
@@ -39,69 +39,69 @@ export default ({ config, db, logger }) => {
       region: config.AWS_REGION
     });
 
-	// Create a new card and if successful return generated cardId
-	api.post('/',
-		validate({
-			body: Joi.object().keys({
-				username: Joi.string().required(),
-				network: Joi.string().required(),
-				language: Joi.string().valid(config.LANGUAGES).required()
-			})
-		}),
-		(req, res, next) => {
-			let cardId = shortid.generate();
-			cards(config, db, logger).create(cardId, req.body)
-				.then((data) => data ? res.status(200).json({ cardId: cardId, created: true }) :
-					next(new Error('Failed to create card')))
-				.catch((err) => {
-					/* istanbul ignore next */
-					logger.error(err);
-					/* istanbul ignore next */
-					next(err);
-				});
-		}
-	);
+  // Create a new card and if successful return generated cardId
+  api.post('/',
+    validate({
+      body: Joi.object().keys({
+        username: Joi.string().required(),
+        network: Joi.string().required(),
+        language: Joi.string().valid(config.LANGUAGES).required()
+      })
+    }),
+    (req, res, next) => {
+      let cardId = shortid.generate();
+      cards(config, db, logger).create(cardId, req.body)
+        .then((data) => data ? res.status(200).json({ cardId: cardId, created: true }) :
+          next(new Error('Failed to create card')))
+        .catch((err) => {
+          /* istanbul ignore next */
+          logger.error(err);
+          /* istanbul ignore next */
+          next(err);
+        });
+    }
+  );
 
-	// Check for the existence of a card
-	api.head('/:cardId', cacheResponse(config.CACHE_DURATION_CARDS),
-		validate({
-			params: { cardId: Joi.string().required() }
-		}),
-		(req, res, next) => {
-			req.apicacheGroup = CACHE_GROUP_CARDS;
-			cards(config, db, logger).byCardId(req.params.cardId)
-				.then((data) => data ? res.status(200).end() : res.status(404).end())
-				.catch((err) => {
-					/* istanbul ignore next */
-					logger.error(err);
-					/* istanbul ignore next */
-					next(err);
-				});
-		}
-	);
+  // Check for the existence of a card
+  api.head('/:cardId', cacheResponse(config.CACHE_DURATION_CARDS),
+    validate({
+      params: { cardId: Joi.string().required() }
+    }),
+    (req, res, next) => {
+      req.apicacheGroup = CACHE_GROUP_CARDS;
+      cards(config, db, logger).byCardId(req.params.cardId)
+        .then((data) => data ? res.status(200).end() : res.status(404).end())
+        .catch((err) => {
+          /* istanbul ignore next */
+          logger.error(err);
+          /* istanbul ignore next */
+          next(err);
+        });
+    }
+  );
 
-	// Return a card
-	api.get('/:cardId', cacheResponse(config.CACHE_DURATION_CARDS),
-		validate({
-			params: { cardId: Joi.string().min(7).max(14).required() }
-		}),
-		(req, res, next) => {
-			req.apicacheGroup = CACHE_GROUP_CARDS;
-			cards(config, db, logger).byCardId(req.params.cardId)
-				.then((data) => handleResponse(data, req, res, next))
-				.catch((err) => {
-					/* istanbul ignore next */
-					logger.error(err);
-					/* istanbul ignore next */
-					next(err);
-				});
-		}
-	);
+  // Return a card
+  api.get('/:cardId', cacheResponse(config.CACHE_DURATION_CARDS),
+    validate({
+      params: { cardId: Joi.string().min(7).max(14).required() }
+    }),
+    (req, res, next) => {
+      req.apicacheGroup = CACHE_GROUP_CARDS;
+      cards(config, db, logger).byCardId(req.params.cardId)
+        .then((data) => handleResponse(data, req, res, next))
+        .catch((err) => {
+          /* istanbul ignore next */
+          logger.error(err);
+          /* istanbul ignore next */
+          next(err);
+        });
+    }
+  );
 
-	// Update a card record with a report
-	api.put('/:cardId', validate({
-		params: { cardId: Joi.string().min(7).max(14).required() },
-		body: Joi.object().keys({
+  // Update a card record with a report
+  api.put('/:cardId', validate({
+    params: { cardId: Joi.string().min(7).max(14).required() },
+    body: Joi.object().keys({
       disaster_type: Joi.string().valid(config.DISASTER_TYPES).required(),
       card_data: Joi.object()
         .keys({
@@ -111,53 +111,53 @@ export default ({ config, db, logger }) => {
         .required()
         .when('disaster_type', {
             is: 'flood',
-            then: Joi.object({ flood_depth: Joi.number().integer().min(0).max(200).required() })		// b.c is required only when a is true
+            then: Joi.object({ flood_depth: Joi.number().integer().min(0).max(200).required() })    // b.c is required only when a is true
         }),
-			text: Joi.string().allow(''),
-			image_url: Joi.string().allow(''),
-			created_at: Joi.date().iso().required(),
-			location: Joi.object().required().keys({
-				lat: Joi.number().min(-90).max(90).required(),
-				lng: Joi.number().min(-180).max(180).required()
-			})
-		})
-	}),
-	(req, res, next) => {
-		try {
-			// First get the card we wish to update
-			cards(config, db, logger).byCardId(req.params.cardId)
-				.then((card) => {
-					// If the card does not exist then return an error message
-					if (!card) res.status(404).json({ statusCode: 404, cardId: req.params.cardId,
-						message: `No card exists with id '${req.params.cardId}'` });
-					// If the card already has received status then return an error message
-					else if (card && card.received) res.status(409).json({ statusCode: 409,
-						cardId: req.params.cardId, message: `Report already received for card '${req.params.cardId}'` });
-					// We have a card and it has not yet had a report received
-					else {
-						// Try and submit the report and update the card
-						cards(config, db, logger).submitReport(card, req.body)
-							.then((data) => {
-								console.log(data);
-								clearCache();
-								res.status(200).json({ statusCode: 200, cardId: req.params.cardId, created: true });
-							})
-							.catch((err) => {
-								/* istanbul ignore next */
-								logger.error(err);
-								/* istanbul ignore next */
-								next(err);
-							});
-					}
-				});
-			} catch(err) {
-				/* istanbul ignore next */
-				logger.error(err);
-				/* istanbul ignore next */
-				next(err);
-			}
-		}
-	);
+      text: Joi.string().allow(''),
+      image_url: Joi.string().allow(''),
+      created_at: Joi.date().iso().required(),
+      location: Joi.object().required().keys({
+        lat: Joi.number().min(-90).max(90).required(),
+        lng: Joi.number().min(-180).max(180).required()
+      })
+    })
+  }),
+  (req, res, next) => {
+    try {
+      // First get the card we wish to update
+      cards(config, db, logger).byCardId(req.params.cardId)
+        .then((card) => {
+          // If the card does not exist then return an error message
+          if (!card) res.status(404).json({ statusCode: 404, cardId: req.params.cardId,
+            message: `No card exists with id '${req.params.cardId}'` });
+          // If the card already has received status then return an error message
+          else if (card && card.received) res.status(409).json({ statusCode: 409,
+            cardId: req.params.cardId, message: `Report already received for card '${req.params.cardId}'` });
+          // We have a card and it has not yet had a report received
+          else {
+            // Try and submit the report and update the card
+            cards(config, db, logger).submitReport(card, req.body)
+              .then((data) => {
+                console.log(data);
+                clearCache();
+                res.status(200).json({ statusCode: 200, cardId: req.params.cardId, created: true });
+              })
+              .catch((err) => {
+                /* istanbul ignore next */
+                logger.error(err);
+                /* istanbul ignore next */
+                next(err);
+              });
+          }
+        });
+      } catch(err) {
+        /* istanbul ignore next */
+        logger.error(err);
+        /* istanbul ignore next */
+        next(err);
+      }
+    }
+  );
 
   //Gives an s3 signed url for the frontend to upload an image to
   api.get('/:cardId/images', validate({
@@ -211,46 +211,46 @@ export default ({ config, db, logger }) => {
     });
   });
 
-	// Update a card report with new details including the image URL
-	api.patch('/:cardId', validate({
-		params: { cardId: Joi.string().min(7).max(14).required() },
-		body: Joi.object().keys({
-			image_url: Joi.string().required()
-		})
-	}),
-	(req, res, next) => {
-		try {
-			// First get the card we wish to update
-			cards(config, db, logger).byCardId(req.params.cardId)
-				.then((card) => {
-					// If the card does not exist then return an error message
-					if (!card) res.status(404).json({ statusCode: 404, cardId: req.params.cardId,
-						message: `No card exists with id '${req.params.cardId}'` });
-					// We have a card
-					else {
-						// Try and submit the report and update the card
-						cards(config, db, logger).updateReport(card, req.body)
-							.then((data) => {
-								console.log(data);
-								clearCache();
-								res.status(200).json({ statusCode: 200, cardId: req.params.cardId, updated: true });
-							})
-							.catch((err) => {
+  // Update a card report with new details including the image URL
+  api.patch('/:cardId', validate({
+    params: { cardId: Joi.string().min(7).max(14).required() },
+    body: Joi.object().keys({
+      image_url: Joi.string().required()
+    })
+  }),
+  (req, res, next) => {
+    try {
+      // First get the card we wish to update
+      cards(config, db, logger).byCardId(req.params.cardId)
+        .then((card) => {
+          // If the card does not exist then return an error message
+          if (!card) res.status(404).json({ statusCode: 404, cardId: req.params.cardId,
+            message: `No card exists with id '${req.params.cardId}'` });
+          // We have a card
+          else {
+            // Try and submit the report and update the card
+            cards(config, db, logger).updateReport(card, req.body)
+              .then((data) => {
+                console.log(data);
+                clearCache();
+                res.status(200).json({ statusCode: 200, cardId: req.params.cardId, updated: true });
+              })
+              .catch((err) => {
                 /* istanbul ignore next */
-								logger.error(err);
-								/* istanbul ignore next */
-								next(err);
-							});
-					}
-				});
-			} catch(err) {
-				/* istanbul ignore next */
-				logger.error(err);
-				/* istanbul ignore next */
-				next(err);
-			}
-		}
-	);
+                logger.error(err);
+                /* istanbul ignore next */
+                next(err);
+              });
+          }
+        });
+      } catch(err) {
+        /* istanbul ignore next */
+        logger.error(err);
+        /* istanbul ignore next */
+        next(err);
+      }
+    }
+  );
 
-	return api;
+  return api;
 };

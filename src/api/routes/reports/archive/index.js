4abb1f26b011b63e8b19d53db0962368b0271950
Tomas <tomasholderness@gmail.com>
2017-06-28 12:22:18 -0400

    added istanbull comments for error handling

--- src/api/routes/reports/archive/index.js
+++ src/api/routes/reports/archive/index.js
@@ -33,7 +33,9 @@ export default ({ config, db, logger }) => {
 		(req, res, next) => archive(config, db, logger).all(req.query.start, req.query.end, req.query.city)
 			.then((data) => handleGeoResponse(data, req, res, next))
 			.catch((err) => {
+				/* istanbul ignore next */
 				logger.error(err);
+				/* istanbul ignore next */
 				next(err);
 			})
 	);

ac9e5f04902da0366f13897c286e2b67c3b27712
Tomas <tomasholderness@gmail.com>
2017-06-29 11:45:12 -0400

    fixed long Joi validation lines

--- src/api/routes/reports/index.js
+++ src/api/routes/reports/index.js
@@ -13,19 +13,23 @@ import Joi from 'joi';
 import validate from 'celebrate';
 
 export default ({config, db, logger}) => {
-	let api = Router();
+	let api = Router(); // eslint-disable-line new-cap
 
 	// Get a list of all reports
 	api.get('/', cacheResponse('1 minute'),
 		validate({
 			query: {
 				city: Joi.any().valid(config.REGION_CODES),
-				timeperiod: Joi.number().integer().positive().max(config.API_REPORTS_TIME_WINDOW_MAX).default(config.API_REPORTS_TIME_WINDOW),
+				timeperiod: Joi.number().integer().positive()
+					.max(config.API_REPORTS_TIME_WINDOW_MAX)
+					.default(config.API_REPORTS_TIME_WINDOW),
 				format: Joi.any().valid(config.FORMATS).default(config.FORMAT_DEFAULT),
-				geoformat: Joi.any().valid(config.GEO_FORMATS).default(config.GEO_FORMAT_DEFAULT),
+				geoformat: Joi.any().valid(config.GEO_FORMATS)
+					.default(config.GEO_FORMAT_DEFAULT),
 			},
 		}),
-		(req, res, next) => reports(config, db, logger).all(req.query.timeperiod, req.query.city)
+		(req, res, next) => reports(config, db, logger)
+													.all(req.query.timeperiod, req.query.city)
 			.then((data) => handleGeoResponse(data, req, res, next))
 			.catch((err) => {
 				/* istanbul ignore next */
@@ -43,8 +47,10 @@ export default ({config, db, logger}) => {
 		validate({
 			params: {id: Joi.number().integer().required()},
 			query: {
-				format: Joi.any().valid(config.FORMATS).default(config.FORMAT_DEFAULT),
-				geoformat: Joi.any().valid(config.GEO_FORMATS).default(config.GEO_FORMAT_DEFAULT),
+				format: Joi.any().valid(config.FORMATS)
+					.default(config.FORMAT_DEFAULT),
+				geoformat: Joi.any().valid(config.GEO_FORMATS)
+					.default(config.GEO_FORMAT_DEFAULT),
 			},
 		}),
 		(req, res, next) => reports(config, db, logger).byId(req.params.id)

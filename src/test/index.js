83424c14ccbda21b986bc596bdb9f6bf17f84e4b
Tomas <tomasholderness@gmail.com>
2017-06-29 14:15:10 -0400

    Fix #41 - removed node-pg from test modules

--- src/test/index.js
+++ src/test/index.js
@@ -7,6 +7,7 @@
 
 // Import config
 import config from '../config';
+config.PGDATABASE = 'cognicity_server_testing';
 
 // Import DB initializer
 import initializeDb from '../db';
@@ -40,12 +41,15 @@ import testCAP from './testCAP.js';
 import testDB from './testDB.js';
 
 // Put some sample data in the database
-const pg = require('pg');
+import Promise from 'bluebird';
+const pgp = require('pg-promise')({
+  promiseLib: Promise, // Use bluebird for enhanced promises
+});
 
 // Create a top-level testing harness
 describe('Cognicity Server Testing Harness', function() {
 // PG config string for dummy data inserts
-let PG_CONFIG_STRING = 'postgres://'+config.PGUSER+'@'+config.PGHOST+':'+config.PGPORT+'/'+config.PGDATABASE;
+const PG_CONFIG_STRING = 'postgres://'+config.PGUSER+'@'+config.PGHOST+':'+config.PGPORT+'/'+config.PGDATABASE;
 
 // Global report value
 let reportid = 1;
@@ -78,174 +82,105 @@ let token = jwt.sign({},
 
     // Removes dummy data
     describe('Cleans up', function() {
-       it('Removes dummy report data', function(done) {
-         let queryObject = {
-           text: `DELETE FROM ${config.TABLE_REPORTS}
-            WHERE source = 'grasp'
-            AND text = 'integration testing';`,
-           values: [reportid],
-         };
-         pg.connect(PG_CONFIG_STRING, function(err, client, pgDone) {
-           client.query(queryObject, function() {
-             if (err) {
-               console.log(err.message);
-               pgDone();
-             } else {
-               pgDone();
-               done();
-             }
-           });
-         });
-       });
+     let db = pgp(PG_CONFIG_STRING);
+     it('Removes dummy report data', function(done) {
+     let query = `DELETE FROM ${config.TABLE_REPORTS}
+          WHERE source = 'grasp'
+          AND text = 'integration testing';`;
 
-       it('Removes dummy cards data', function(done) {
-         let queryObject = {
-           text: `DELETE FROM ${config.TABLE_GRASP_CARDS}
-            WHERE username = 'testuser'
-            AND network = 'test network';`,
-           values: [reportid],
-         };
-         pg.connect(PG_CONFIG_STRING, function(err, client, pgDone) {
-           client.query(queryObject, function() {
-             if (err) {
-               console.log(err.message);
-               pgDone();
-             } else {
-               pgDone();
-               done();
-             }
-           });
-         });
-       });
+       db.none(query)
+        .then(() => {
+          done();
+        })
+        .catch((error) => console.log(error));
+     });
 
-       it('Removes dummy cards data', function(done) {
-         let queryObject = {
-           text: `DELETE FROM ${config.TABLE_GRASP_REPORTS}
-            WHERE text = 'integration testing';`,
-           values: [reportid],
-         };
-         pg.connect(PG_CONFIG_STRING, function(err, client, pgDone) {
-           client.query(queryObject, function() {
-             if (err) {
-               console.log(err.message);
-               pgDone();
-             } else {
-               pgDone();
-               done();
-             }
-           });
-         });
-       });
+     it('Removes dummy cards data', function(done) {
+       let query = `DELETE FROM ${config.TABLE_GRASP_CARDS}
+          WHERE username = 'testuser'
+          AND network = 'test network';`;
+
+        db.none(query)
+          .then(() => {
+            done();
+          })
+          .catch((error) => console.log(error));
+     });
+
+     it('Removes dummy cards data', function(done) {
+       let query = `DELETE FROM ${config.TABLE_GRASP_REPORTS}
+          WHERE text = 'integration testing';`;
+
+      db.none(query)
+        .then(() => {
+          done();
+        })
+        .catch((error) => console.log(error));
+     });
 
      // Remove dummy data from REM floods table
      it('Removes dummy flood data', function(done) {
-      let queryObject = {
-         text: `DELETE FROM ${config.TABLE_REM_STATUS} WHERE local_area = 5`,
-       };
-       pg.connect(PG_CONFIG_STRING, function(err, client, pgDone) {
-         client.query(queryObject, function() {
-           if (err) {
-             console.log(err.message);
-             pgDone();
-           } else {
-             pgDone();
-             done();
-           }
-         });
-       });
+      let query = `DELETE FROM ${config.TABLE_REM_STATUS} WHERE local_area = 5`;
+
+      db.none(query)
+        .then(() => {
+          done();
+        })
+        .catch((error) => console.log(error));
      });
 
      // Remove dummy data from REM floods table
      it('Removes dummy flood data from log', function(done) {
-      let queryObject = {
-         text: `DELETE FROM ${config.TABLE_REM_STATUS_LOG}
-          WHERE username = testing`,
-       };
-       pg.connect(PG_CONFIG_STRING, function(err, client, pgDone) {
-         client.query(queryObject, function() {
-           if (err) {
-             console.log(err.message);
-             pgDone();
-           } else {
-             pgDone();
-             done();
-           }
-         });
+      let query = `DELETE FROM ${config.TABLE_REM_STATUS_LOG}
+                    WHERE username = 'testing'`;
+        db.none(query)
+          .then(() => {
+            done();
+          })
+          .catch((error) => console.log(error));
        });
-     });
+
 
      // Remove dummy data from qlue table
      it('Removes dummy qlue data', function(done) {
-      let queryObject = {
-         text: `DELETE FROM ${config.TABLE_FEEDS_QLUE} WHERE pkey = 9999`,
-       };
-       pg.connect(PG_CONFIG_STRING, function(err, client, pgDone) {
-         client.query(queryObject, function() {
-           if (err) {
-             console.log(err.message);
-             pgDone();
-           } else {
-             pgDone();
-             done();
-           }
-         });
-       });
+      let query =`DELETE FROM ${config.TABLE_FEEDS_QLUE} WHERE pkey = 9999`;
+      db.none(query)
+        .then(() => {
+          done();
+        })
+        .catch((error) => console.log(error));
      });
 
      // Remove dummy qlue data from all reports table
      it('Removes dummy qlue data', function(done) {
-      let queryObject = {
-         text: `DELETE FROM ${config.TABLE_REPORTS}
-          WHERE fkey = 9999 AND source = 'qlue'`,
-       };
-       pg.connect(PG_CONFIG_STRING, function(err, client, pgDone) {
-         client.query(queryObject, function() {
-           if (err) {
-             console.log(err.message);
-             pgDone();
-           } else {
-             pgDone();
-             done();
-           }
-         });
-       });
+      let query = `DELETE FROM ${config.TABLE_REPORTS}
+          WHERE fkey = 9999 AND source = 'qlue'`;
+      db.none(query)
+        .then(() => {
+          done();
+        })
+        .catch((error) => console.log(error));
      });
 
      // Remove dummy data from detik table
      it('Removes dummy detik data', function(done) {
-      let queryObject = {
-         text: `DELETE FROM ${config.TABLE_FEEDS_DETIK} WHERE pkey = 9999`,
-       };
-       pg.connect(PG_CONFIG_STRING, function(err, client, pgDone) {
-         client.query(queryObject, function() {
-           if (err) {
-             console.log(err.message);
-             pgDone();
-           } else {
-             pgDone();
-             done();
-           }
-         });
+      let query = `DELETE FROM ${config.TABLE_FEEDS_DETIK} WHERE pkey = 9999`;
+      db.none(query)
+        .then(() => {
+          done();
+        })
+        .catch((error) => console.log(error));
        });
-     });
 
      // Remove dummy detik data from all reports table
      it('Removes dummy detik data', function(done) {
-      let queryObject = {
-         text: `DELETE FROM ${config.TABLE_REPORTS}
-          WHERE fkey = 9999 AND source = 'detik'`,
-       };
-       pg.connect(PG_CONFIG_STRING, function(err, client, pgDone) {
-         client.query(queryObject, function() {
-           if (err) {
-             console.log(err.message);
-             pgDone();
-           } else {
-             pgDone();
-             done();
-           }
-         });
-       });
+      let query = `DELETE FROM ${config.TABLE_REPORTS}
+          WHERE fkey = 9999 AND source = 'detik'`;
+      db.none(query)
+        .then(() => {
+          done();
+        })
+        .catch((error) => console.log(error));
      });
      return (done());
    });

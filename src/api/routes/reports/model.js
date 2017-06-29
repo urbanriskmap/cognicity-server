9da38fae18dbb76cbed419e5b88cb99b450f5c48
Tomas <tomasholderness@gmail.com>
2017-06-29 11:44:53 -0400

    fixed long query lines

--- src/api/routes/reports/model.js
+++ src/api/routes/reports/model.js
@@ -6,12 +6,13 @@ export default (config, db, logger) => ({
 	 * Return all reports within a defined time period, and optionally city
 	 * @param {integer} timeperiod Length of time period in seconds
 	 * @param {string} city Optional, instance region code (e.g. 'jbd')
+	 * @return {Object} Query results
 	 */
 	all: (timeperiod, city) => new Promise((resolve, reject) => {
 		// Setup query
 		let query = `SELECT pkey, created_at, source,
-			status, url, image_url, disaster_type, report_data, tags, title, text, the_geom
-			FROM ${config.TABLE_REPORTS}
+			status, url, image_url, disaster_type, report_data, tags, title, text,
+			the_geom FROM ${config.TABLE_REPORTS}
 			WHERE created_at >= to_timestamp($1)
 			AND ($2 IS NULL OR tags->>'instance_region_code'=$2)
 			ORDER BY created_at DESC LIMIT $3`;
@@ -35,8 +36,8 @@ export default (config, db, logger) => ({
 	byId: (id) => new Promise((resolve, reject) => {
 		// Setup query
 		let query = `SELECT pkey, created_at, source,
-			status, url, image_url, disaster_type, report_data, tags, title, text, the_geom
-			FROM ${config.TABLE_REPORTS}
+			status, url, image_url, disaster_type, report_data, tags, title, text,
+			the_geom FROM ${config.TABLE_REPORTS}
 			WHERE pkey = $1`;
 
 		// Setup values

ff666164951b98295cb7483e2fc24f299e4f8d69
Tomas <tomasholderness@gmail.com>
2017-06-28 20:52:18 -0400

    disabled multi string check

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,18 @@
 module.exports = {
     "extends": ["eslint:recommended", "google"],
-    "parserOptions": { "ecmaVersion": 6, "sourceType": "module" },
-    "env": { "es6": true, "node": true, "mocha": true }
+    "parserOptions":
+      {
+        "ecmaVersion": 6,
+        "sourceType": "module"
+      },
+    "env":
+      { "browser": false,
+        "es6": true,
+        "node": true,
+        "mocha": true
+      },
+    "rules":
+      {
+        "no-multi-str": "off" // not a problem in node apps
+      }
 };

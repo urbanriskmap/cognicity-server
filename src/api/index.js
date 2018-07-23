f6bfdda3832205e14e72c1f0e6ad4fbf0cfbb155
Tomas <tomasholderness@gmail.com>
2018-07-23 16:35:04 -0400

    log entire request object

--- src/api/index.js
+++ src/api/index.js
@@ -35,7 +35,7 @@ export default ({config, db, logger}) => {
 
   // Return the API version
   api.get('/', (req, res) => {
-    console.log(req.headers); // eslint-disable-line no-console
+    console.log(req); // eslint-disable-line no-console
     let query = `SELECT * FROM cognicity.version()`;
     db.oneOrNone(query)
       .then((data) => {
